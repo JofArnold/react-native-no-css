a307b2b651777983382f55248a6b3ae9e58737e3
Jof Arnold <github@jofarnold.com>
2018-02-22 14:13:12 +0000

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended', 'prettier', 'plugin:react/recommended'],
+  plugins: ['prettier', 'react'],
+  env: {
+    jest: true,
+    es6: true,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+      },
+    ],
+    'comma-dangle': [
+      2,
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'never',
+      },
+    ],
+    'no-confusing-arrow': 0,
+    'no-console': 0,
+    'no-else-return': 0,
+    'no-underscore-dangle': 0,
+    'no-unused-vars': [
+      2,
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-restricted-syntax': 0,
+    'no-await-in-loop': 0,
+    camelcase: 0,
+  },
+};
