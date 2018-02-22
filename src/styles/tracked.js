7852b8ccc1b9f1d304d0327a24623ef5c0a998e6
Jof Arnold <github@jofarnold.com>
2018-02-22 14:59:56 +0000

    Add useful tracked style

--- src/styles/tracked.js
+++ src/styles/tracked.js
@@ -4,6 +4,7 @@ const scale = {
   tracked: 0.1,
   "tracked-tight": -0.05,
   "tracked-mega": 0.25,
+  "tracked-light": 0.03,
 };
 
 export default _.mapValues(scale, val => ({ letterSpacing: val }));
