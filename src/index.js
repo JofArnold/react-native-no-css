f12768f52b25e5939491c252790b1db21affc2af
Jof Arnold <github@jofarnold.com>
2018-02-22 15:15:07 +0000

    Import colors

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import opacity from "./styles/opacity";
 import * as absolute from "./styles/absolute";
 import lineHeight from "./styles/lineHeight";
 import tracked from "./styles/tracked";
+import { defaultColors } from "./styles/defaultColors";
 
 const NativeTachyons = {
   wrap: reactWrapper.wrap,
@@ -41,6 +42,7 @@ const NativeTachyons = {
     _.assign(styleSheet, images);
     _.assign(styleSheet, text);
     _.assign(styleSheet, opacity);
+    _.assign(styleSheet, defaultColors);
 
     /* Calculate rem scales */
     const sizes = {};
